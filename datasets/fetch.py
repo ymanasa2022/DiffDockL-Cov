0b83d9860e78a3a56d8d7041b5e2cb0d8de145b0
Manasa Yadavalli <ymanasa@umich.edu>
2024-12-26 11:43:18 -0500

    can fetch af2 models of proteins that have invalid aa

--- datasets/fetch.py
+++ datasets/fetch.py
@@ -11,17 +11,20 @@ def get_uniprot_id(pdb_id, pretty=False):
     # pull out the UniProt id. for this PDB id:
     uniprot_id = json_results[pdb_id] 
     uniprot_id2 = uniprot_id["UniProt"]
-    uniprot_id3 = list(uniprot_id2.keys()) # a list of the UniProt ids. for this input PDB id.
-    assert(len(uniprot_id3) == 1)
-    uniprot_id4 = uniprot_id3[0]
-    print("UniProt id=",uniprot_id4)
+    uniprot_id3 = list(uniprot_id2.keys()) # a list of the UniProt ids
+    # handeling multiple uniprots 
+    if len(uniprot_id3) == 1:
+        uniprot_id4 = uniprot_id3[0]
+        return uniprot_id4
+    elif len(uniprot_id3) > 1:
+        print(f"Multiple UniProt IDs found for {pdb_id}: {uniprot_id3}")
+    else:
+        print(f"No UniProt ID found for: {pdb_id}")
 
-    return uniprot_id4
-
-def fetch_af2(pdb_id, output_dir, suffix=None):
+def fetch_af2(pdb_id, output_path_file):
     '''
     get AlphaFold2 structure using PDB ID
-    suffix: for naming the pdb file  
+    output_path_file: path to file *.pdb
     '''
     uniprot_id = get_uniprot_id(pdb_id)
 
@@ -29,7 +32,7 @@ def fetch_af2(pdb_id, output_dir, suffix=None):
 
     response = requests.get(af2_url)
     if response.status_code == 200:
-        with open(f"{output_dir}/{pdb_id}{suffix}.pdb", "wb") as f:
+        with open(f"{output_path_file}", "wb") as f:
             f.write(response.content)
         print(f"AlphaFold2 structure for {uniprot_id} downloaded successfully!")
     else:
